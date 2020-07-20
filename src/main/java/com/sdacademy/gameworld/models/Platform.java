1fcffc5732e6264e69e3bec839387d35d33bd3e7
Patryk Kożuchowski <patryk.kozuchowski11@gmail.com>
2020-07-20 15:27:17 +0200

    change for spring

--- src/main/java/com/sdacademy/gameworld/models/Platform.java
+++
@@ -1,7 +0,0 @@
-package com.sdacademy.gameworld.models;
-
-public enum Platform {
-
-    PC, PS3, PS4, PS5, XBOX_360, XBOX_ONE
-
-}
--- src/main/java/com/sdacademy/gameworld/models/StatusOrder.java
+++
@@ -1,7 +0,0 @@
-package com.sdacademy.gameworld.models;
-
-public enum StatusOrder {
-
-    CANCELED, FINISHED, IN_PROGRESS
-
-}
