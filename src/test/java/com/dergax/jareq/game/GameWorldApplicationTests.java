167321ad043f433da295ab421bdf4e817a552c34
Patryk Kożuchowski <patryk.kozuchowski11@gmail.com>
2020-07-20 15:41:28 +0200

    startv1

---
+++ src/test/java/com/dergax/jareq/game/GameWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dergax.jareq.game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
