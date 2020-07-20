be89bb908b1f5b9bd44afd2c17b0c1027415adbd
Patryk Kożuchowski <patryk.kozuchowski11@gmail.com>
2020-07-20 15:13:45 +0200

    change for spring

--- src/main/java/com/dergax_and_jareq/game/entity/Platform.java
+++
@@ -1,7 +0,0 @@
-package com.dergax_and_jareq.game.entity;
-
-public enum Platform {
-
-    PC, PS3, PS4, PS5, XBOX_360, XBOX_ONE, XBOX_SERIES_X
-
-}
--- src/main/java/com/dergax_and_jareq/game/entity/StatusOrder.java
+++
@@ -1,7 +0,0 @@
-package com.dergax_and_jareq.game.entity;
-
-public enum StatusOrder {
-
-    CANCELED, FINISHED, IN_PROGRESS
-
-}
